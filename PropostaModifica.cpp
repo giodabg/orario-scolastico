b2665ae5349702eed8ac33164c723869abf4e00c
BONDPC\diego <diego.bondesan08@gmail.com>
2026-02-08 19:45:39 +0100

    Revert "creazione del metodo FromXML della classe propostaModifiche"
    This reverts commit 0df1f7b45bf587c6c4551468037bed62fad6c5f2.

--- PropostaModifica.cpp
+++ PropostaModifica.cpp
@@ -1,4 +1,4 @@
-si#include "PropostaModifica.h"
+#include "PropostaModifica.h"
 #include "Orario.h"
 using namespace std;
 
@@ -26,31 +26,8 @@ string PropostaModifica::toXML()
     return xml;
 }
 
-void PropostaModifica::fromXML(string NomeFile)
+void Orario::fromXML()
 {
-        try
-        {
-            if (!File.Exists(NomeFile))
-            {
-                cout << NomeFile + " non trovato";
-                return;
-            }
-
-            using StreamReader sr = new StreamReader(NomeFile);
-            var risultato = myXML.Deserialize(sr) as List<PropostaModifica>;
-            if (risultato == null)
-            {
-                cout << "XML non valido";
-                return;
-            }
-
-            PropostaModifica = risultato;
-            refreshList();
-        }
-        catch (Exception ex)
-        {
-            cout << "ERRORE:\n" + ex.Message;
-        }
 }
 
 void PropostaModifica::fromXML(std::string nomeFile)
